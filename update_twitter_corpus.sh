17dba8ff41520a4f825c5aeee2654f2f5ff1b7be
Guenter Bartsch <guenter@zamia.org>
2019-06-24 22:46:51 +0200

    add date, use newspaper instead of bs4+readability

--- update_twitter_corpus.sh
+++ update_twitter_corpus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./twitterscrape.py -v -s 2019-06-01 corpus201906 /home/bofh/projects/ai/data/corpora/en/twitter/twitter_sources.txt
+./twitterscrape.py -v -s 2019-06-01 twitter201906 /home/bofh/projects/ai/data/corpora/en/twitter/twitter_sources.txt
 
