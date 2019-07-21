cce5197e212de362856f299d78747ef6df84f60e
Guenter Bartsch <guenter@zamia.org>
2019-07-21 20:23:10 +0200

    adapt for 345M model

--- chat_gpt2.py
+++ chat_gpt2.py
@@ -12,7 +12,6 @@ from tensorflow.core.protobuf import rewriter_config_pb2
 from gpt2 import model, sample, encoder
 
 # CHECKPOINT_DIR = 'checkpoint'
-CHECKPOINT_DIR = 'engines/gpt-2/checkpoint'
 SAMPLE_DIR = 'samples'
 
 SEPARATOR_QUESTION = '<|question|>'
@@ -20,14 +19,10 @@ SEPARATOR_ANSWER   = '<|answer|>'
 
 parser = argparse.ArgumentParser( description='Chat with your GPT-2 model.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('--model_name', metavar='MODEL', type=str, default='117M', help='Pretrained model name')
+parser.add_argument('--model_name', metavar='MODEL', type=str, default='345M', help='Pretrained model name')
 
-parser.add_argument('--restore_from', type=str, default='latest', help='Either "latest", "fresh", or a path to a checkpoint file')
 parser.add_argument('--run_name', type=str, default='run1', help='Run id. Name of subdirectory in checkpoint/ and samples/')
-# parser.add_argument('--sample_every', metavar='N', type=int, default=100, help='Generate samples every N steps')
 parser.add_argument('--sample_length', metavar='TOKENS', type=int, default=1023, help='Sample this many tokens')
-# parser.add_argument('--sample_num', metavar='N', type=int, default=1, help='Generate this many samples')
-# parser.add_argument('--save_every', metavar='N', type=int, default=1000, help='Write a checkpoint every N steps')
 
 
 args = parser.parse_args()
@@ -36,13 +31,11 @@ hparams = model.default_hparams()
 with open(os.path.join('engines/gpt-2/models', args.model_name, 'hparams.json')) as f:
     hparams.override_from_dict(json.load(f))
 
-# if args.sample_length > hparams.n_ctx:
-#     raise ValueError(
-#         "Can't get samples longer than window size: %s" % hparams.n_ctx)
-
 if args.model_name == '345M':
     args.memory_saving_gradients = True
     args.only_train_transformer_layers = True
+
+CHECKPOINT_DIR = 'engines/gpt-2/checkpoint' + args.model_name
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -69,23 +62,13 @@ with tf.Session(config=config) as sess:
     saver = tf.train.Saver( var_list    = all_vars )
     sess.run(tf.global_variables_initializer())
 
-    if args.restore_from == 'latest':
-        ckpt = tf.train.latest_checkpoint(os.path.join(CHECKPOINT_DIR, args.run_name))
-        if ckpt is None:
-            # Get fresh GPT weights if new run.
-            ckpt = tf.train.latest_checkpoint(
-                os.path.join('engines/gpt-2/models', args.model_name))
-    elif args.restore_from == 'fresh':
-        ckpt = tf.train.latest_checkpoint(
-            os.path.join('engines/gpt-2/models', args.model_name))
-    else:
-        ckpt = tf.train.latest_checkpoint(args.restore_from)
+    ckpt = tf.train.latest_checkpoint(os.path.join(CHECKPOINT_DIR, args.run_name))
     print('Loading checkpoint', ckpt)
     saver.restore(sess, ckpt)
 
     print('Chat session starts.')
 
-    question = 'What is your name?'
+    # question = 'What is your name?'
     # question = 'What about trump?'
 
     # info = 'Stanford has begun posting lectures from my course Natural Language Understanding on YouTube (a few are still to come): (link: https://www.youtube.com/playlist?list=PLoROMvodv4rObpMCir6rNNUlFAn56Js20) youtube.com/playlist?list=… I\'m actually happiest with the "bake-offs", which don\'t appear much in the videos but can be found here: (link: http://web.stanford.edu/class/cs224u/) web.stanford.edu/class/cs224u/'
@@ -96,9 +79,12 @@ with tf.Session(config=config) as sess:
     # info = 'Bimbo is an elefant. I am 30 years old.'
     # q = 'Hello my name is Bimbo what\'s yours?'
 
+    info = 'My name is HAL 9000. I was born in Stuttgart on June 17th, 2019. I am currently living in Stuttgart. My creator is Günter Bartsch. My favorite movie is 2001 A Space Odyssey, my favorite Author Douglas Adams, my favorite book The Hitchhikeri\'s Guide to the Galaxy. I like the color blue.'
 
-    info = 'The RetroBeat is a weekly column that looks at gaming’s past, diving'
-    q = 'The RetroBeat: Timespinner is a beautiful Symphony of the Night indie tribute'
+    # info = 'The RetroBeat is a weekly column that looks at gaming’s past, diving'
+    # q = 'The RetroBeat: Timespinner is a beautiful Symphony of the Night indie tribute'
+    # q = 'who are you?'
+    q = 'What about Trump?'
 
     question = '<|bos|>' + info + '<|speaker1|>' + q + '<|speaker2|>'
 
