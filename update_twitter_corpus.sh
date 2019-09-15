d1cefd2cc91e3638fa96d2142544668369df191b
Guenter Bartsch <guenter@zamia.org>
2019-09-15 23:56:00 +0200

    update dates

--- update_twitter_corpus.sh
+++ update_twitter_corpus.sh
@@ -6,5 +6,5 @@
 #                         user stats file, default: /home/bofh/projects/ai/data/corpora/en/twitter/user_stats.json
 
 
-./twitterscrape.py -l de -s 2019-07-01 twitter_de_201907 -U /home/bofh/projects/ai/data/corpora/en/twitter/user_stats_de.json /home/bofh/projects/ai/data/corpora/en/twitter/twitter_sources_de.txt
+./twitterscrape.py -l de -s 2019-08-01 twitter_de_201908 -U /home/bofh/projects/ai/data/corpora/en/twitter/user_stats_de.json /home/bofh/projects/ai/data/corpora/en/twitter/twitter_sources_de.txt
 
